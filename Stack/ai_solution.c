fb53b479706a95f57e051eab7efeffc5cc87af60
ainunria <ainunriaar@gmail.com>
2025-05-16 11:28:36 +0800

    Update ai_solution.c

--- Stack/ai_solution.c
+++ Stack/ai_solution.c
@@ -1,76 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
 
-#define MAX 100
+void deleteMid(stack<int>& st)
+{
+    int n = st.size();
+    stack<int> tempSt;
+    int count = 0;
 
-typedef struct {
-    int data[MAX];
-    int top;
-} Stack;
-
-// Operasi dasar
-void push(Stack* s, int val) {
-    if (s->top == MAX - 1) {
-        printf("Stack overflow\n");
-        return;
+    while (count < n / 2) {
+        int c = st.top();
+        st.pop();
+        tempSt.push(c);
+        count++;
     }
-    s->data[++s->top] = val;
-}
 
-int pop(Stack* s) {
-    if (s->top == -1) {
-        printf("Stack underflow\n");
-        exit(1);
+    st.pop();
+
+    while (!tempSt.empty()) {
+        st.push(tempSt.top());
+        tempSt.pop();
     }
-    return s->data[s->top--];
 }
 
-int empty(Stack* s) {
-    return s->top == -1;
-}
+int main()
+{
+    stack<int> st;
 
-// Hapus elemen tengah secara rekursif
-void deleteMiddle(Stack* s, int current, int middle) {
-    if (empty(s))
-        return;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
 
-    int temp = pop(s);
+    deleteMid(st);
 
-    if (current != middle)
-        deleteMiddle(s, current + 1, middle);
-    else
-        return; // Jangan push kembali elemen tengah
-
-    push(s, temp);
-}
-
-// Cetak isi stack
-void printStack(Stack* s) {
-    while (!empty(s)) {
-        printf("%d ", pop(s));
+    while (!st.empty()) {
+        int p = st.top();
+        st.pop();
+        cout << p << " ";
     }
-    printf("\n");
-}
-
-// Fungsi utama
-int main() {
-    Stack s;
-    s.top = -1;
-
-    // Push elemen ke stack: 10, 20, 30, 40, 50
-    push(&s, 10);
-    push(&s, 20);
-    push(&s, 30);
-    push(&s, 40);
-    push(&s, 50);
-
-    int n = s.top + 1;         // Banyak elemen
-    int mid = n / 2;           // Posisi tengah (0-based)
-
-    deleteMiddle(&s, 0, mid);  // Hapus elemen tengah
-
-    // Output stack setelah penghapusan
-    printStack(&s); // Output: 50 40 20 10
-
     return 0;
 }
